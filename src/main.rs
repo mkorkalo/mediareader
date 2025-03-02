67f9fd296b39e430e484a35f544abf932763dbbd
Mikko Korkalo <mikko.korkalo@sensorfleet.com>
2025-03-02 04:15:45 +0200

    implement actual path

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use std::{
 async fn main() {
     let app = Router::new()
         .route("/", get(|| async { Html("Hello") }))
-        .route("/media/{server}/{id_a}/{id_b}/{id_c}", get(get_media));
+        .route("/_matrix/media/v1/download/{server}/{id}", get(get_media));
 let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     axum_server::bind(addr)
         .serve(app.into_make_service())
@@ -24,13 +24,20 @@ let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
         .unwrap();
 }
 
-async fn get_media(Path((_server, id_a, id_b, id_c)): Path<(String, String, String, String)>) -> impl IntoResponse {
-    let root_path = StdPath::new("media");
-    let media_path = root_path.join(id_a).join(id_b).join(id_c);
+async fn get_media(Path((_server, id)): Path<(String, String)>) -> impl IntoResponse {
+    if id.len() != "aabbcccccccccccccccccccc".len() {
+        return (StatusCode::BAD_REQUEST, Bytes::from("Invalid ID length"));
+    }
+    let id_a = &id[0..2];
+    let id_b = &id[2..4];
+    let id_c = &id[4..];
+    let root_path = StdPath::new("/media/local_content");
+    let media_path = root_path.
+        join(id_a).join(id_b).join(id_c);
     println!("Reading file: {}", media_path.display());
     let exists = fs::exists(media_path.as_path()).unwrap();
     if !exists {
-        return (StatusCode::NOT_FOUND, Bytes::from(format!("File does not exist")));
+        return (StatusCode::NOT_FOUND, Bytes::from("File does not exist"));
     };
     return match fs::read(media_path) {
         Ok(data) => (StatusCode::OK, Bytes::from(data)),
